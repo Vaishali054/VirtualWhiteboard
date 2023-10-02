552d8156cb9c17c7549694dc6632362c29dba43f
shivani sharma <vanis131203@gmail.com>
2023-10-02 21:46:29 +0530

    layout

--- src/Component/slides/Slides.jsx
+++ src/Component/slides/Slides.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from "react";
 import './slides.css';
 
 export default function Slides() {
@@ -7,24 +7,23 @@ export default function Slides() {
 
   useEffect(() => {
     showSlides(slideIndex);
-  }, [slideIndex]);
+  });
 
   const addWritingSlide = () => {
     if (!firstSlideAdded) {
-      // Creating writing board for the first slide
+      // Creating a writing board for the first slide
       createWritingBoard();
       setFirstSlideAdded(true);
     } else {
       setSlideIndex(slideIndex + 1);
-      const slideElement = document.createElement('canvas');
-      slideElement.textContent = `Writing Slide ${slideIndex}`;
-      slideElement.classList.add('writing-slide');
-      document.getElementById('slide-container').appendChild(slideElement);
+      const canvasElement = document.createElement('canvas');
+      canvasElement.classList.add('writing-slide');
+      document.getElementById('slide-container').appendChild(canvasElement);
     }
   };
 
   const createWritingBoard = () => {
-    slideIndex++;
+    setSlideIndex(slideIndex + 1);
     const canvasElement = document.createElement('canvas');
     canvasElement.classList.add('writing-slide');
     document.getElementById('slide-container').appendChild(canvasElement);
@@ -36,44 +35,35 @@ export default function Slides() {
 
   const showSlides = (index) => {
     const slides = document.getElementsByClassName('writing-slide');
-
+  
+    if (slides.length === 0) {
+      // No 'writing-slide' elements found, handle this case as needed
+      return;
+    }
+  
     if (index < 1) {
       setSlideIndex(1);
     } else if (index > slides.length) {
       setSlideIndex(slides.length);
     }
+  
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
+  
     if (slides.length > 0) {
       slides[slideIndex - 1].style.display = 'block';
     }
   };
+  
 
   return (
     <>
-     <div>
+      <div>
         <div className="container">
           <nav className="navbar fixed-top navbar-expand-sm bg-light">
-            <a href="#" className="navbar-brand mb-0 h1">
-              <img
-                className="d-inline-block align-top"
-                src="images/logo.png"
-                alt="logo"
-                width="40"
-                height="40"
-              />
-            </a>
-            <button
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              className="navbar-toggler"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
+            <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" className="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
@@ -84,15 +74,8 @@ export default function Slides() {
                 </li>
                 {/* Add New Slide Dropdown */}
                 <li className="nav-item dropdown">
-                  <a
-                    href="#"
-                    className="nav-link dropdown-toggle"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Add New Slide
+                  <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Add New Slide
                   </a>
                   {/* Dropdown Menu */}
                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -100,32 +83,26 @@ export default function Slides() {
                     <li className="dropdown-item">
                       {/* Navigation controls for slides */}
                       <span className="d-flex">
-                        <a
-                          className="prev"
-                          onClick={() => this.plusSlides(-1)}
-                        >
+                        <button className="prev btn btn-outline-dark" onClick={() => plusSlides(-1)}>
                           &#10094;
-                        </a>
+                        </button>
                         <button
                           id="add-slide-button"
                           type="button"
                           className="btn btn-light"
-                          onClick={this.addWritingSlide}
+                          onClick={addWritingSlide}
                         >
                           New Slide
                         </button>
-                        <a
-                          className="next"
-                          onClick={() => this.plusSlides(1)}
-                        >
+                        <button className="next btn btn-outline-dark" onClick={() => plusSlides(1)}>
                           &#10095;
-                        </a>
+                        </button>
                       </span>
                     </li>
                   </ul>
                 </li>
                 <li className="nav-item">
-                  <a href="#" className="nav-link ">
+                  <a className="nav-link">
                     Profile
                   </a>
                 </li>
@@ -135,7 +112,8 @@ export default function Slides() {
         </div>
         <br />
         <div className="main-container">
-          <canvas id="slide-container"></canvas>
+          <canvas id="slide-container">
+          </canvas>
         </div>
       </div>
     </>
