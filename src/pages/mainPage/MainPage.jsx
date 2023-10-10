bb539eb489cfcaad977928783e407400b305a55f
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-10-10 14:50:20 +0530

    removing unecessary imports

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Slides from '../../components/slides/Slides'
-import ColorPalette from '../../components/Palette/ColorPalette'
+// import Slides from '../../components/slides/Slides'
+// import ColorPalette from '../../components/Palette/ColorPalette'
 import Navbar from '../../components/navbar/Navbar'
 
 export default function MainPage() {
